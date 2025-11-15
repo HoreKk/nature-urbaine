24feea3190a4959afcb22f5cbcb524adbeac4db2
Antoine Lelong <azenoxe@gmail.com>
2025-11-15 20:40:51 +0100

    feat(chore): first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Nature Urbaine</title>
+				<meta content="Bienvenue sur Nature Urbaine" name="description" />
+				<link href="/favicon.ico" rel="icon" />
+			</Head>
+			<main>
+				<h1>Main page</h1>
+			</main>
+		</>
+	);
+}
