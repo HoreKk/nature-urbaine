523c86b12b93849262513638ed2e4e23a0b57eaf
Antoine Lelong <azenoxe@gmail.com>
2026-04-25 20:09:54 +0200

    fix: add migrations files

---
+++ packages/database/src/migrations/20260425_180937.ts
@@ -0,0 +1,323 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   CREATE TYPE "public"."enum_reports_season" AS ENUM('spring', 'summer', 'autumn', 'winter');
+  CREATE TYPE "public"."enum_reports_status" AS ENUM('draft', 'published');
+  CREATE TYPE "public"."enum__reports_v_version_season" AS ENUM('spring', 'summer', 'autumn', 'winter');
+  CREATE TYPE "public"."enum__reports_v_version_status" AS ENUM('draft', 'published');
+  CREATE TABLE "media" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"alt" varchar NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"url" varchar,
+  	"thumbnail_u_r_l" varchar,
+  	"filename" varchar,
+  	"mime_type" varchar,
+  	"filesize" numeric,
+  	"width" numeric,
+  	"height" numeric,
+  	"focal_x" numeric,
+  	"focal_y" numeric
+  );
+  
+  CREATE TABLE "reports" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"thumbnail_id" integer,
+  	"name" varchar,
+  	"description" varchar,
+  	"location" geometry(Point),
+  	"category_id" integer,
+  	"date" timestamp(3) with time zone,
+  	"season" "enum_reports_season",
+  	"city_stratum" varchar,
+  	"nb_populations" numeric,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"_status" "enum_reports_status" DEFAULT 'draft'
+  );
+  
+  CREATE TABLE "_reports_v" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"parent_id" integer,
+  	"version_thumbnail_id" integer,
+  	"version_name" varchar,
+  	"version_description" varchar,
+  	"version_location" geometry(Point),
+  	"version_category_id" integer,
+  	"version_date" timestamp(3) with time zone,
+  	"version_season" "enum__reports_v_version_season",
+  	"version_city_stratum" varchar,
+  	"version_nb_populations" numeric,
+  	"version_updated_at" timestamp(3) with time zone,
+  	"version_created_at" timestamp(3) with time zone,
+  	"version__status" "enum__reports_v_version_status" DEFAULT 'draft',
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"latest" boolean
+  );
+  
+  CREATE TABLE "interviews" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"interviewee" varchar NOT NULL,
+  	"interviewee_role" varchar NOT NULL,
+  	"city" varchar NOT NULL,
+  	"department" varchar NOT NULL,
+  	"area" varchar NOT NULL,
+  	"project_owner" varchar NOT NULL,
+  	"project_management" varchar NOT NULL,
+  	"summary" varchar NOT NULL,
+  	"project_details_objectives" jsonb NOT NULL,
+  	"project_details_impacts" jsonb NOT NULL,
+  	"project_details_challenges" jsonb NOT NULL,
+  	"realised_at" timestamp(3) with time zone NOT NULL,
+  	"published_at" timestamp(3) with time zone NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE "pictures" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"alt" varchar NOT NULL,
+  	"report_id" integer NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"url" varchar,
+  	"thumbnail_u_r_l" varchar,
+  	"filename" varchar,
+  	"mime_type" varchar,
+  	"filesize" numeric,
+  	"width" numeric,
+  	"height" numeric,
+  	"focal_x" numeric,
+  	"focal_y" numeric,
+  	"sizes_thumbnail_url" varchar,
+  	"sizes_thumbnail_width" numeric,
+  	"sizes_thumbnail_height" numeric,
+  	"sizes_thumbnail_mime_type" varchar,
+  	"sizes_thumbnail_filesize" numeric,
+  	"sizes_thumbnail_filename" varchar
+  );
+  
+  CREATE TABLE "pictures_rels" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"order" integer,
+  	"parent_id" integer NOT NULL,
+  	"path" varchar NOT NULL,
+  	"tags_id" integer
+  );
+  
+  CREATE TABLE "categories" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"description" varchar,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE "tags" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"description" varchar,
+  	"tag_category_id" integer NOT NULL,
+  	"parent_id_id" integer,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE "tag_categories" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"description" varchar,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE "users_sessions" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"created_at" timestamp(3) with time zone,
+  	"expires_at" timestamp(3) with time zone NOT NULL
+  );
+  
+  CREATE TABLE "users" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"email" varchar NOT NULL,
+  	"reset_password_token" varchar,
+  	"reset_password_expiration" timestamp(3) with time zone,
+  	"salt" varchar,
+  	"hash" varchar,
+  	"login_attempts" numeric DEFAULT 0,
+  	"lock_until" timestamp(3) with time zone
+  );
+  
+  CREATE TABLE "payload_kv" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"key" varchar NOT NULL,
+  	"data" jsonb NOT NULL
+  );
+  
+  CREATE TABLE "payload_locked_documents" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"global_slug" varchar,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE "payload_locked_documents_rels" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"order" integer,
+  	"parent_id" integer NOT NULL,
+  	"path" varchar NOT NULL,
+  	"media_id" integer,
+  	"reports_id" integer,
+  	"interviews_id" integer,
+  	"pictures_id" integer,
+  	"categories_id" integer,
+  	"tags_id" integer,
+  	"tag_categories_id" integer,
+  	"users_id" integer
+  );
+  
+  CREATE TABLE "payload_preferences" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"key" varchar,
+  	"value" jsonb,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE "payload_preferences_rels" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"order" integer,
+  	"parent_id" integer NOT NULL,
+  	"path" varchar NOT NULL,
+  	"users_id" integer
+  );
+  
+  CREATE TABLE "payload_migrations" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"name" varchar,
+  	"batch" numeric,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  ALTER TABLE "reports" ADD CONSTRAINT "reports_thumbnail_id_media_id_fk" FOREIGN KEY ("thumbnail_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "reports" ADD CONSTRAINT "reports_category_id_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "public"."categories"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "_reports_v" ADD CONSTRAINT "_reports_v_parent_id_reports_id_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."reports"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "_reports_v" ADD CONSTRAINT "_reports_v_version_thumbnail_id_media_id_fk" FOREIGN KEY ("version_thumbnail_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "_reports_v" ADD CONSTRAINT "_reports_v_version_category_id_categories_id_fk" FOREIGN KEY ("version_category_id") REFERENCES "public"."categories"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "pictures" ADD CONSTRAINT "pictures_report_id_reports_id_fk" FOREIGN KEY ("report_id") REFERENCES "public"."reports"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "pictures_rels" ADD CONSTRAINT "pictures_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."pictures"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "pictures_rels" ADD CONSTRAINT "pictures_rels_tags_fk" FOREIGN KEY ("tags_id") REFERENCES "public"."tags"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "tags" ADD CONSTRAINT "tags_tag_category_id_tag_categories_id_fk" FOREIGN KEY ("tag_category_id") REFERENCES "public"."tag_categories"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "tags" ADD CONSTRAINT "tags_parent_id_id_tags_id_fk" FOREIGN KEY ("parent_id_id") REFERENCES "public"."tags"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "users_sessions" ADD CONSTRAINT "users_sessions_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."payload_locked_documents"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_media_fk" FOREIGN KEY ("media_id") REFERENCES "public"."media"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_reports_fk" FOREIGN KEY ("reports_id") REFERENCES "public"."reports"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_interviews_fk" FOREIGN KEY ("interviews_id") REFERENCES "public"."interviews"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_pictures_fk" FOREIGN KEY ("pictures_id") REFERENCES "public"."pictures"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_categories_fk" FOREIGN KEY ("categories_id") REFERENCES "public"."categories"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_tags_fk" FOREIGN KEY ("tags_id") REFERENCES "public"."tags"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_tag_categories_fk" FOREIGN KEY ("tag_categories_id") REFERENCES "public"."tag_categories"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_users_fk" FOREIGN KEY ("users_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_preferences_rels" ADD CONSTRAINT "payload_preferences_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."payload_preferences"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "payload_preferences_rels" ADD CONSTRAINT "payload_preferences_rels_users_fk" FOREIGN KEY ("users_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+  CREATE INDEX "media_updated_at_idx" ON "media" USING btree ("updated_at");
+  CREATE INDEX "media_created_at_idx" ON "media" USING btree ("created_at");
+  CREATE UNIQUE INDEX "media_filename_idx" ON "media" USING btree ("filename");
+  CREATE INDEX "reports_thumbnail_idx" ON "reports" USING btree ("thumbnail_id");
+  CREATE INDEX "reports_category_idx" ON "reports" USING btree ("category_id");
+  CREATE INDEX "reports_updated_at_idx" ON "reports" USING btree ("updated_at");
+  CREATE INDEX "reports_created_at_idx" ON "reports" USING btree ("created_at");
+  CREATE INDEX "reports__status_idx" ON "reports" USING btree ("_status");
+  CREATE INDEX "_reports_v_parent_idx" ON "_reports_v" USING btree ("parent_id");
+  CREATE INDEX "_reports_v_version_version_thumbnail_idx" ON "_reports_v" USING btree ("version_thumbnail_id");
+  CREATE INDEX "_reports_v_version_version_category_idx" ON "_reports_v" USING btree ("version_category_id");
+  CREATE INDEX "_reports_v_version_version_updated_at_idx" ON "_reports_v" USING btree ("version_updated_at");
+  CREATE INDEX "_reports_v_version_version_created_at_idx" ON "_reports_v" USING btree ("version_created_at");
+  CREATE INDEX "_reports_v_version_version__status_idx" ON "_reports_v" USING btree ("version__status");
+  CREATE INDEX "_reports_v_created_at_idx" ON "_reports_v" USING btree ("created_at");
+  CREATE INDEX "_reports_v_updated_at_idx" ON "_reports_v" USING btree ("updated_at");
+  CREATE INDEX "_reports_v_latest_idx" ON "_reports_v" USING btree ("latest");
+  CREATE INDEX "interviews_updated_at_idx" ON "interviews" USING btree ("updated_at");
+  CREATE INDEX "interviews_created_at_idx" ON "interviews" USING btree ("created_at");
+  CREATE INDEX "pictures_report_idx" ON "pictures" USING btree ("report_id");
+  CREATE INDEX "pictures_updated_at_idx" ON "pictures" USING btree ("updated_at");
+  CREATE INDEX "pictures_created_at_idx" ON "pictures" USING btree ("created_at");
+  CREATE UNIQUE INDEX "pictures_filename_idx" ON "pictures" USING btree ("filename");
+  CREATE INDEX "pictures_sizes_thumbnail_sizes_thumbnail_filename_idx" ON "pictures" USING btree ("sizes_thumbnail_filename");
+  CREATE INDEX "pictures_rels_order_idx" ON "pictures_rels" USING btree ("order");
+  CREATE INDEX "pictures_rels_parent_idx" ON "pictures_rels" USING btree ("parent_id");
+  CREATE INDEX "pictures_rels_path_idx" ON "pictures_rels" USING btree ("path");
+  CREATE INDEX "pictures_rels_tags_id_idx" ON "pictures_rels" USING btree ("tags_id");
+  CREATE INDEX "categories_updated_at_idx" ON "categories" USING btree ("updated_at");
+  CREATE INDEX "categories_created_at_idx" ON "categories" USING btree ("created_at");
+  CREATE INDEX "tags_tag_category_idx" ON "tags" USING btree ("tag_category_id");
+  CREATE INDEX "tags_parent_id_idx" ON "tags" USING btree ("parent_id_id");
+  CREATE INDEX "tags_updated_at_idx" ON "tags" USING btree ("updated_at");
+  CREATE INDEX "tags_created_at_idx" ON "tags" USING btree ("created_at");
+  CREATE INDEX "tag_categories_updated_at_idx" ON "tag_categories" USING btree ("updated_at");
+  CREATE INDEX "tag_categories_created_at_idx" ON "tag_categories" USING btree ("created_at");
+  CREATE INDEX "users_sessions_order_idx" ON "users_sessions" USING btree ("_order");
+  CREATE INDEX "users_sessions_parent_id_idx" ON "users_sessions" USING btree ("_parent_id");
+  CREATE INDEX "users_updated_at_idx" ON "users" USING btree ("updated_at");
+  CREATE INDEX "users_created_at_idx" ON "users" USING btree ("created_at");
+  CREATE UNIQUE INDEX "users_email_idx" ON "users" USING btree ("email");
+  CREATE UNIQUE INDEX "payload_kv_key_idx" ON "payload_kv" USING btree ("key");
+  CREATE INDEX "payload_locked_documents_global_slug_idx" ON "payload_locked_documents" USING btree ("global_slug");
+  CREATE INDEX "payload_locked_documents_updated_at_idx" ON "payload_locked_documents" USING btree ("updated_at");
+  CREATE INDEX "payload_locked_documents_created_at_idx" ON "payload_locked_documents" USING btree ("created_at");
+  CREATE INDEX "payload_locked_documents_rels_order_idx" ON "payload_locked_documents_rels" USING btree ("order");
+  CREATE INDEX "payload_locked_documents_rels_parent_idx" ON "payload_locked_documents_rels" USING btree ("parent_id");
+  CREATE INDEX "payload_locked_documents_rels_path_idx" ON "payload_locked_documents_rels" USING btree ("path");
+  CREATE INDEX "payload_locked_documents_rels_media_id_idx" ON "payload_locked_documents_rels" USING btree ("media_id");
+  CREATE INDEX "payload_locked_documents_rels_reports_id_idx" ON "payload_locked_documents_rels" USING btree ("reports_id");
+  CREATE INDEX "payload_locked_documents_rels_interviews_id_idx" ON "payload_locked_documents_rels" USING btree ("interviews_id");
+  CREATE INDEX "payload_locked_documents_rels_pictures_id_idx" ON "payload_locked_documents_rels" USING btree ("pictures_id");
+  CREATE INDEX "payload_locked_documents_rels_categories_id_idx" ON "payload_locked_documents_rels" USING btree ("categories_id");
+  CREATE INDEX "payload_locked_documents_rels_tags_id_idx" ON "payload_locked_documents_rels" USING btree ("tags_id");
+  CREATE INDEX "payload_locked_documents_rels_tag_categories_id_idx" ON "payload_locked_documents_rels" USING btree ("tag_categories_id");
+  CREATE INDEX "payload_locked_documents_rels_users_id_idx" ON "payload_locked_documents_rels" USING btree ("users_id");
+  CREATE INDEX "payload_preferences_key_idx" ON "payload_preferences" USING btree ("key");
+  CREATE INDEX "payload_preferences_updated_at_idx" ON "payload_preferences" USING btree ("updated_at");
+  CREATE INDEX "payload_preferences_created_at_idx" ON "payload_preferences" USING btree ("created_at");
+  CREATE INDEX "payload_preferences_rels_order_idx" ON "payload_preferences_rels" USING btree ("order");
+  CREATE INDEX "payload_preferences_rels_parent_idx" ON "payload_preferences_rels" USING btree ("parent_id");
+  CREATE INDEX "payload_preferences_rels_path_idx" ON "payload_preferences_rels" USING btree ("path");
+  CREATE INDEX "payload_preferences_rels_users_id_idx" ON "payload_preferences_rels" USING btree ("users_id");
+  CREATE INDEX "payload_migrations_updated_at_idx" ON "payload_migrations" USING btree ("updated_at");
+  CREATE INDEX "payload_migrations_created_at_idx" ON "payload_migrations" USING btree ("created_at");`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   DROP TABLE "media" CASCADE;
+  DROP TABLE "reports" CASCADE;
+  DROP TABLE "_reports_v" CASCADE;
+  DROP TABLE "interviews" CASCADE;
+  DROP TABLE "pictures" CASCADE;
+  DROP TABLE "pictures_rels" CASCADE;
+  DROP TABLE "categories" CASCADE;
+  DROP TABLE "tags" CASCADE;
+  DROP TABLE "tag_categories" CASCADE;
+  DROP TABLE "users_sessions" CASCADE;
+  DROP TABLE "users" CASCADE;
+  DROP TABLE "payload_kv" CASCADE;
+  DROP TABLE "payload_locked_documents" CASCADE;
+  DROP TABLE "payload_locked_documents_rels" CASCADE;
+  DROP TABLE "payload_preferences" CASCADE;
+  DROP TABLE "payload_preferences_rels" CASCADE;
+  DROP TABLE "payload_migrations" CASCADE;
+  DROP TYPE "public"."enum_reports_season";
+  DROP TYPE "public"."enum_reports_status";
+  DROP TYPE "public"."enum__reports_v_version_season";
+  DROP TYPE "public"."enum__reports_v_version_status";`)
+}
---
+++ packages/database/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260425_180937 from './20260425_180937';
+
+export const migrations = [
+  {
+    up: migration_20260425_180937.up,
+    down: migration_20260425_180937.down,
+    name: '20260425_180937'
+  },
+];
