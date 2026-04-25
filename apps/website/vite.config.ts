32dc688e6fa9edc6d6b728523ac06f863b0977cf
Antoine Lelong <azenoxe@gmail.com>
2026-04-25 17:18:57 +0200

    fix: upgrade version of vite and tanstack on website apps

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -5,7 +5,6 @@ import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const config = defineConfig({
 	resolve: {
@@ -17,10 +16,6 @@ const config = defineConfig({
 		devtools(),
 		// nitroV3Plugin(),
 		nitroV2Plugin(),
-		// this is the plugin that enables path aliases
-		viteTsConfigPaths({
-			projects: ['./tsconfig.json'],
-		}),
 		tanstackStart(),
 		viteReact(),
 	],
