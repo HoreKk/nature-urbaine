262f3fc2f0b36bb4846a07f4320a51ebb41e5d1e
Antoine Lelong <azenoxe@gmail.com>
2026-04-30 00:18:40 +0200

    fix: reset migrations to one file for pg_geometry issue

--- packages/database/src/migrations/index.ts
+++ packages/database/src/migrations/index.ts
@@ -1,21 +1,9 @@
-import * as migration_20260425_180937 from './20260425_180937';
-import * as migration_20260428_133107 from './20260428_133107';
-import * as migration_20260429_220845 from './20260429_220845';
+import * as migration_20260429_221801 from './20260429_221801';
 
 export const migrations = [
   {
-    up: migration_20260425_180937.up,
-    down: migration_20260425_180937.down,
-    name: '20260425_180937',
-  },
-  {
-    up: migration_20260428_133107.up,
-    down: migration_20260428_133107.down,
-    name: '20260428_133107',
-  },
-  {
-    up: migration_20260429_220845.up,
-    down: migration_20260429_220845.down,
-    name: '20260429_220845'
+    up: migration_20260429_221801.up,
+    down: migration_20260429_221801.down,
+    name: '20260429_221801'
   },
 ];
